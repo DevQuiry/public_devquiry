6049b6acc08e17d66da1175c5eb020693019d2bf
Dan Trickey <dan@metasysta.co.uk>
2015-06-28 10:18:52 -0400

    Initial Commit with files from new stack

---
+++ app/programs/server/app/devquiry.js
@@ -0,0 +1,12 @@
+(function(){Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
+
+})();
---
+++ config/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PORT=80
+export MONGO_URL=mongodb://127.0.0.1/devquiry
+export ROOT_URL=http://localhost
+
+#it is possible to override above env-vars from the user-provided values
+
+  export ROOT_URL=\h\t\t\p\:\/\/\1\0\4\.\1\3\1\.\1\8\8\.\2\4\8
+
+  export METEOR_SETTINGS=\{\"\p\u\b\l\i\c\"\:\{\}\}
+
+  export CLUSTER_ENDPOINT_URL=\h\t\t\p\:\/\/\1\0\4\.\1\3\1\.\1\8\8\.\2\4\8\:\8\0
+
